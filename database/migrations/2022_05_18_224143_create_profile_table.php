28049dc774195ec4d79efe574ebfa8dd155b5a30
John James <ospina.john@correounivalle.edu.co>
2022-05-19 15:36:32 -0500

    Tablas creadas, relacion creada entre usuario,perfil y rol

---
+++ database/migrations/2022_05_18_224143_create_profile_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profile', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('status');
+            $table->integer('user_id')->unsigned();
+            $table->integer('rol_id')->unsigned();
+
+            #referencia de rol
+            $table->foreign('rol_id')
+                ->references('id')
+                ->on('rol');
+
+            #referencia de usuario
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profile');
+    }
+};
